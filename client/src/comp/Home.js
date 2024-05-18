445001a5f415104c042410beb26feb3c8ac81d96
Vishnu Vardhan <163332747+vishnuvardhan0902@users.noreply.github.com>
2024-05-18 17:02:35 +0530

    Update Home.js

--- client/src/comp/Home.js
+++ client/src/comp/Home.js
@@ -4,8 +4,8 @@ import './Home.css'; // Importing CSS file for styling
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="blog-name">TrekEase</h1><div className='container bg-white'>
-      <p className="intro-text bg-white">Welcome to TrekEase, your ultimate destination for travel inspiration and adventure! Embark on a journey with us as we explore breathtaking landscapes, vibrant cultures, and hidden gems around the globe. Whether you're a seasoned globetrotter or a novice explorer, our curated content will ignite your wanderlust and provide invaluable tips for your next expedition. From awe-inspiring nature hikes to bustling urban escapades, TrekEase is your compass to discovering the world's wonders. Join our community of fellow travelers and share your experiences, tips, and memorable moments. Let's embark on unforgettable adventures together!</p></div>
+      <h1 className="blog-name">BlogVerse</h1><div className='container bg-white'>
+      <p className="intro-text bg-white">In the Blogverse, embark on epic adventures with TrekEase! Explore breathtaking landscapes, vibrant cultures, and hidden gems with our travel inspiration. Join our community and share your wanderlust - together we'll launch into unforgettable journeys!</p></div>
     </div>
   );
 }
